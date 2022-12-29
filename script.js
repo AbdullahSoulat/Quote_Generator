fe7b484a0aad7c778a3f0416445cd5eb85e0bfd4
AbdullahSoulat <abdullahsoulat40@gmail.com>
2022-12-29 15:55:43 +0500

    added author name and better quotes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const quote = document.getElementById('insert-quote');
+const author = document.getElementById('author');
 
 const getNewQuote = () => {
-    fetch("https://api.kanye.rest")
+    fetch("https://api.quotable.io/random")
       .then((response) => response.json())
-      .then((data) => (quote.innerHTML = data.quote));
+      .then((data) => {
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+      });
   };
 
 
